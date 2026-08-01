6731f28af4d9cb2b09ad008f1fdc2bc3580b4d01
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-01 14:48:33 +0530

    exception handling

---
+++ Exception_Handling/src/com/exception/handling/ExceptionHandling.java
@@ -0,0 +1,21 @@
+package com.exception.handling;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+		
+		System.out.println("ExceptionHandling.main()......START");
+		
+		int balance = 200;
+		String name = null;
+		
+		System.out.println(balance);
+		System.out.println(name.length());
+		
+		
+		
+		System.out.println("ExceptionHandling.main()......END");
+
+	}
+
+}
