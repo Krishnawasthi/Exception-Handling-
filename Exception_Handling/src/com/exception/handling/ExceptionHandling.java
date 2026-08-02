c44b3264b6c9163ecad4e6359addfa03e3f2ace0
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-02 19:09:03 +0530

    exception

--- Exception_Handling/src/com/exception/handling/ExceptionHandling.java
+++ Exception_Handling/src/com/exception/handling/ExceptionHandling.java
@@ -2,18 +2,45 @@ package com.exception.handling;
 
 public class ExceptionHandling {
 
+	  static  int somevalue;
 	public static void main(String[] args) {
 		
 		System.out.println("ExceptionHandling.main()......START");
 		
-		int balance = 200;
 		String name = null;
 		
-		System.out.println(balance);
-		System.out.println(name.length());
 		
 		
 		
+		
+		// we will put the exptected exception problem in try and block and throws it via catch block so there will be no abnormal termination
+		
+		try 
+		{   
+			int amount = 200/0;
+			System.out.println("amount" + amount); // ArithmeticException occurs
+			
+			System.out.println("before exception");
+			System.out.println(name.length()); //Risky may throw -->NullPointerException
+			System.out.println("after exception");
+		}
+		
+	  catch(NullPointerException e) 
+		{
+		   e.printStackTrace();
+		   
+		   System.out.println("Name is null....");
+			
+		}
+		catch(ArithmeticException e1) 
+		{
+		   e1.printStackTrace();
+		   
+		   System.out.println("A number can not zero");
+			
+		}
+		
+					
 		System.out.println("ExceptionHandling.main()......END");
 
 	}
