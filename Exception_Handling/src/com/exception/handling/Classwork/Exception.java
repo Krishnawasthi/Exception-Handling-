04db004ca7de6d97614c60138dd617389380a543
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-03 09:08:49 +0530

    new one

---
+++ Exception_Handling/src/com/exception/handling/Classwork/Exception.java
@@ -0,0 +1,18 @@
+package com.exception.handling.Classwork;
+
+public class Exception {
+                                                            
+	public static void main(String[] args) {
+		
+		String name = args[0];   //arithmatic operation when we donot provide the arguments
+		
+		
+		//String animal = null;
+		
+		//System.out.println(animal.length());   //NullPointer exception
+		
+	   System.out.println(name);
+	
+	
+	}
+}
