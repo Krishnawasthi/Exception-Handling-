7e2f059549b4cddc07a9014f3e6133f87bd1f827
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-03 11:24:57 +0530

    chnge

--- Exception_Handling/src/com/exception/handling/Classwork/Exception.java
+++ Exception_Handling/src/com/exception/handling/Classwork/Exception.java
@@ -1,18 +1,65 @@
 package com.exception.handling.Classwork;
 
-public class Exception {
+public class Exception extends Object {
                                                             
 	public static void main(String[] args) {
-		
-		String name = args[0];   //arithmatic operation when we donot provide the arguments
-		
-		
-		//String animal = null;
-		
-		//System.out.println(animal.length());   //NullPointer exception
-		
-	   System.out.println(name);
+		System.out.println("Exception.main() ...start");
 	
+		
+		//String name =;   //arithmatic operation when we donot provide the arguments
+		
+		
+		//null pointer exception  
+		try
+		{
+			
+		String name = "krishna";
+		
+		String animal = args[0];
+		
+		int number = 200/2;
+		
+		System.out.println(name.length());//NullPointer exception
+		
+		
+		System.out.println(animal.charAt(2));
+		
+		System.out.println(number);
+		
+		Exception e = new Exception();
+		e.doSomething();
+		}
+		
+		catch(NullPointerException e) {
+			
+			System.out.println("Exception : name is null");
+       	e.printStackTrace();   // this is used to print the actual exception where is the problem
+		}
+		
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			
+			System.out.println("Exception : array index are not found");
+			e.printStackTrace();
+		}
+	  
+		catch(ArithmeticException e) {
+			
+			
+			System.out.println("Exception :Arithmatic operator");
+			e.printStackTrace();
+		}
+				
+				
+		
+		System.out.println("Exception.main()..END"); 
+	   
+	}
 	
+   private void doSomething() {
+		
+		System.out.println("Exception.doSothing()....START");
+		System.out.println("Exception.doSothing()....doing something");
+		System.out.println("Exception.doSothing()....END");
 	}
 }
