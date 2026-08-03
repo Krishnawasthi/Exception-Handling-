cd213ac48ee6c594c3a81d2b0080ffa052fd64e4
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-03 23:35:16 +0530

    day2

--- Exception_Handling/src/com/exception/handlingday2/Driver.java
+++ Exception_Handling/src/com/exception/handlingday2/Driver.java
@@ -5,8 +5,13 @@ public class Driver
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 
+		System.out.println("Driver.main().............START");
+		Order order = new Order();
+
+		order.doSomething();
+
+		System.out.println("Driver.main().............END");
 	}
 
 }
--- Exception_Handling/src/com/exception/handlingday2/Order.java
+++ Exception_Handling/src/com/exception/handlingday2/Order.java
@@ -2,5 +2,28 @@ package com.exception.handlingday2;
 
 public class Order
 {
+	String name = "krishna"; // asssume recieve from another call
+    String Animalname = "dog";
+	public void doSomething()
+	{
+		try
+		{
+			System.out.println(name.charAt(3)); // risky code
+			System.out.println("after the element at index for name ");
+			System.out.println(Animalname.charAt(3));  //String index out of the bound exception
+			System.out.println("after the element at index for animalname ");
+		}
 
+		catch (Exception e)
+		{
+		    name= "NA";
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < 5; i++)
+		{
+			System.out.println("Order.doSomething()" + i);
+		}
+
+	}
 }
